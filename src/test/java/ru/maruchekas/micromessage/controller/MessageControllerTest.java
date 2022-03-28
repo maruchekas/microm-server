a362b0e5ffc1a205f08e8e52d9e7276ae90c91e8
maruchekas <salken84@gmail.com>
2022-03-28 08:41:36 +0300

    updated tests

--- src/test/java/ru/maruchekas/micromessage/controller/MessageControllerTest.java
+++ src/test/java/ru/maruchekas/micromessage/controller/MessageControllerTest.java
@@ -8,18 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.maruchekas.micromessage.AbstractTest;
 import ru.maruchekas.micromessage.api.request.CreateMessageRequest;
+import ru.maruchekas.micromessage.repository.UserRepository;
 import ru.maruchekas.micromessage.service.MessageService;
 
 import java.time.LocalDateTime;
 
-import static ru.maruchekas.micromessage.config.Constants.*;
-
+import static ru.maruchekas.micromessage.config.Constants.INCORRECT_PERIOD;
+import static ru.maruchekas.micromessage.config.Constants.INVALID_DATEFORMAT;
 
 
 @SpringBootTest
@@ -29,6 +32,12 @@ public class MessageControllerTest extends AbstractTest {
 
     @Autowired
     MessageService messageService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void setup() {
@@ -41,11 +50,12 @@ public class MessageControllerTest extends AbstractTest {
 
 
     @Test
+    @WithMockUser(username = "admin@mail.ru", authorities = "user:write")
     void sendMessageTest() throws Exception {
         CreateMessageRequest createMessageRequest = new CreateMessageRequest()
                 .setText("test text");
 
-        String ctime = LocalDateTime.now().toString().substring(0,17);
+        String ctime = LocalDateTime.now().toString().substring(0, 17);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/message")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -57,6 +67,20 @@ public class MessageControllerTest extends AbstractTest {
     }
 
     @Test
+    @WithMockUser(username = "user@mail.ru", authorities = "user:read")
+    void sendMessageBadTest() throws Exception {
+        CreateMessageRequest createMessageRequest = new CreateMessageRequest()
+                .setText("test text");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/message")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createMessageRequest)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "user@mail.ru", authorities = "user:read")
     void getMessageListTest() throws Exception {
 
         String fromTime = LocalDateTime.now().minusDays(1L).toString();
@@ -71,6 +95,7 @@ public class MessageControllerTest extends AbstractTest {
     }
 
     @Test
+    @WithMockUser(username = "user@mail.ru", authorities = "user:read")
     void getMessageListBadTest() throws Exception {
 
         String fromTime = LocalDateTime.now().minusDays(1L).toString();
@@ -82,11 +107,12 @@ public class MessageControllerTest extends AbstractTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.total").doesNotExist())
-                .andExpect(MockMvcResultMatchers.jsonPath("$").value(INVALID_DATEFORMAT))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(INVALID_DATEFORMAT))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
     @Test
+    @WithMockUser(username = "user@mail.ru", authorities = "user:read")
     void getMessageListBadRangeTest() throws Exception {
 
         String fromTime = LocalDateTime.now().toString();
@@ -97,7 +123,7 @@ public class MessageControllerTest extends AbstractTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.total").doesNotExist())
-                .andExpect(MockMvcResultMatchers.jsonPath("$").value(INCORRECT_PERIOD))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(INCORRECT_PERIOD))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
